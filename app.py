a46b453694514d06617407aa346c5e2dc3d2ff07
bxptech <bxptechnical@gmail.com>
2025-08-12 16:45:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ embeddings = GoogleGenerativeAIEmbeddings(
 )
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.5-pro",
-    temperature=0,
+    temperature=0.3,
     google_api_key=os.getenv("GOOGLE_API_KEY")
 )
 
